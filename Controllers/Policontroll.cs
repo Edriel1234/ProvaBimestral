9e444ef08c3ebb5fb8b85a55fb00b6b6a84d3ddc
Edriel Mendonça <edrirl43@gmail.com>
2020-09-22 20:12:09 -0300

    prova bimestral

---
+++ Controllers/Policontroll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProvaBimestral.Models;
+
+namespace ProvaBimestral.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Policontroll : ControllerBase
+    {
+        [HttpGet]
+        public async Task <IActionResult> Get(int id)
+        {
+            return Ok("Dados  ");
+        } 
+
+        [HttpPost]
+        public async Task <IActionResult> Post(Politico politico)
+        {
+            return Ok(" Político cadastrado");
+        }
+    }
+}
---
+++ Models/Poli.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaBimestral.Models
+{
+    public class Poli
+    {
+        public int Id { get; set; }
+        public string NomeCandidato { get; set; }
+        public string NomePartido { get; set; }
+        public string NumeroPolitico  { get; set; }
+        public string CargoPolitico { get; set; }
+        public string Foto { get; set; }
+
+    }
+}
